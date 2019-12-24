7f656bb4e2a21d5f9ef0e3c33fa8c7a7cd131a21
Igor Berlenko <igor@berlenko.com>
2019-12-24 19:13:50 +0800

    initial implementation

---
+++ plugins/telegram.js
@@ -0,0 +1,22 @@
+const TelegramBot = require('node-telegram-bot-api')
+const fp = require('fastify-plugin')
+const dotenv = require('dotenv').config()
+
+const token = process.env.BOT_TOKEN
+const chatId = process.env.CHAT_ID
+
+module.exports = fp(function (fastify, opts, next) {
+  const telegram = new TelegramBot(token, { polling: false })
+
+  fastify.decorate('sendTelegramMessage', function (title, body, image, account) {
+    const message = `\n
+      #gooddeed by ${account} \n
+      *${title}* \n
+
+      ${body} \n
+    `
+
+    telegram.sendPhoto(chatId, image, { caption: message, "contentType": "image/png", parse_mode: "Markdown" } ).catch(err => { console.error(err) })
+  })
+  next()
+})
---
+++ services/root.js
@@ -0,0 +1,42 @@
+'use strict'
+
+module.exports = function (fastify, opts, next) {
+  fastify.get('/', function (request, reply) {
+    reply.send({ root: true })
+  })
+
+  fastify.post('/', function (request, reply) {
+    try {
+      const body = JSON.parse(request.body)
+    
+      console.log(body)
+
+      const messageTitle = body['title']
+      const messageBody = body['partial_body']
+      const messageImage = body['image']
+      const messageAccount = body['account_name']
+      
+      fastify.sendTelegramMessage(messageTitle, messageBody, messageImage, messageAccount)
+  
+      reply.send({
+        success: true
+      })
+    } catch (err) {
+      console.error(err)
+
+      reply.send({
+        success: false
+      })
+    }
+  })
+
+  next()
+}
+
+// If you prefer async/await, use the following
+//
+// module.exports = async function (fastify, opts) {
+//   fastify.get('/', async function (request, reply) {
+//     return { root: true }
+//   })
+// }
